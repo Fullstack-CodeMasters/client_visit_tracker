d04026a9f1659bb798749b61f48c704aa8877077
Minu K Alex <A-8958@ibsplc.com>
2019-12-03 17:21:37 +0530

    meals page updation Signed-off-by: Minu K Alex

--- src/components/forms/Meals.js
+++ src/components/forms/Meals.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Form, Row, Col,Container } from "react-bootstrap";
+import { Form, Row, Col,Container,ButtonToolbar, Button} from "react-bootstrap";
 
 export class Meals extends Component {
     continue = e => {
@@ -23,36 +23,46 @@ export class Meals extends Component {
     //     console.log(data);
     // }
 render() {
-var names = ['21-08-2019'];
 const { values, handleChange } = this.props;
+console.log(values.MealsDetails);
+var meals = values.MealsDetails;
 return (
     <Container>
         <Row>
             <Col md={12}>
                 <h1>Meals Details</h1>
-                <Form.Row>
-                    <Form.Group as={Col} md="6" controlId="date">
-                        <Form.Label>date</Form.Label>
-                        <Form.Control name="date" type="date" placeholder="Enter date"/>
-                    </Form.Group>
-
-                    <Form.Group as={Col} md="6" controlId="arrangeFrom">
+                {meals.map(function (meal, index) {
+                     return (
+                        <React.Fragment>
+                <ButtonToolbar>
+                    <Button variant="primary" size="sm" active>
+                    {meal.date}
+                    </Button>
+            
+                </ButtonToolbar>
+                <Form.Row>  
+                    <Form.Group as={Col} md="4" controlId="arrangeFrom">
                         <Form.Label>Arrange From</Form.Label>
-                        <Form.Control name="arrangeFrom" type="arrangeFrom" placeholder="Arrange From"/>
-                    </Form.Group>
-                </Form.Row>
-                <Form.Row>   
-                    <Form.Group  as={Col} md="6" controlId="veg_cnt">
+                        <Form.Control name="arrangeFrom" type="arrangeFrom" placeholder="Arrange From"  onChange={handleChange('meal.arrangeFrom')}
+              defaultValue={meal.arrangeFrom}/>
+
+                    </Form.Group> 
+                    <Form.Group  as={Col} md="4" controlId="veg_cnt">
                         <Form.Label>Veg Count</Form.Label>
-                        <Form.Control name="veg_cnt" type="veg_cnt" placeholder="Veg Count"/>
+                        <Form.Control name="veg_cnt" type="veg_cnt" placeholder="Veg Count" onChange={handleChange('meal.vegCount')} defaultValue={meal.vegCount} required/>
                     </Form.Group>
-                    <Form.Group  as={Col} md="6" controlId="nonveg_cnt">
+                    <Form.Group  as={Col} md="4" controlId="nonveg_cnt">
                         <Form.Label>Nonveg Count</Form.Label>
-                        <Form.Control name="nonveg_cnt" type="nonveg_cnt" placeholder="Nonveg Count"/>
+                        <Form.Control name="nonveg_cnt" type="nonveg_cnt" placeholder="Nonveg Count" onChange={handleChange('meal.nonVegCount')} defaultValue={meal.nonVegCount} required/>
                     </Form.Group>
-        ​        </Form.Row>               
-                <button onClick={this.back} >Back</button>
-                <button onClick={this.continue}>Continue</button>
+        ​        </Form.Row>     
+        </React.Fragment>
+           ) 
+            })}     
+               <ButtonToolbar>
+                <Button variant="primary"  onClick={this.back}>Back</Button>&nbsp;
+                <Button variant="secondary" onClick={this.continue}>Continue</Button>
+                </ButtonToolbar>
             </Col>
         </Row>
     </Container>
